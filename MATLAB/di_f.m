1f636f03f6996ee94210b3fb0f51f3e67ca8adf5
Strumpa <raphael.guasch@polymtl.ca>
2024-05-06 17:50:06 -0400

    added MOC related scripts, next build solution in MOC_2D_CARCEL.m script

--- MATLAB/di_f.m
+++
@@ -1,9 +0,0 @@
-% function f=di_f(tau0,sig,segment)
-% (c) 2008 Alain Hebert, Ecole Polytechnique de Montreal
-function f=di_f(sig,segment)
-    if sig ~= 0
-        f=segment/sig-(akin(3,0)-akin(3,sig*segment))/sig^2 ;
-    else
-        f=pi*segment^2/4 ;
-    end
-    
--- MATLAB/ei_f.m
+++
@@ -1,8 +0,0 @@
-function f=ei_f(tau0,sig,segment)
-% function f=ei_f(tau0,sig,segment)
-% (c) 2008 Alain Hebert, Ecole Polytechnique de Montreal
-  if sig ~= 0
-    f=(akin(3,tau0)-akin(3,tau0+sig*segment))/sig ;
-  else
-    f=segment*akin(2,tau0) ;
-  end
